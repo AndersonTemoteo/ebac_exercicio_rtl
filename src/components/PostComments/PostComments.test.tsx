431f828f60020e88a1aee5a2650dfe96ff8037ba
AndersonTemoteo <andersontemoteobezerra@gmail.com>
2024-08-08 17:36:46 -0300

    Mudanças no projeto para realizar o exercício.

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,23 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
+import userEvent from '@testing-library/user-event'
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
+    test('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    test('Deve inserir dois comentários corretamente', () => {
+        render(<PostComment/>);
+        const botao = screen.getByTestId('btn-inserir-comentario')
+
+        for (let i = 1; i <= 2; i++) {
+            userEvent.type(screen.getByTestId('txt-area'), 'comentario '+ i)
+            fireEvent.click(botao)
+        }
+        
+        expect(screen.getByText('comentario 1')).toBeInTheDocument()
+        expect(screen.getByText('comentario 2')).toBeInTheDocument()
+    })
 });
